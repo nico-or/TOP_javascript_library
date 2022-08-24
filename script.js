84beee42bc31c9607785d1f8cc70566f22f8e838
Nicolás Órdenes <nicordenes@gmail.com>
2022-08-24 14:30:52 -0400

    Add Book.toTableRow method
    Adds Book method to create a table row html element

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
+const library = []
+
 function Book(title, author, pages, status) {
   this.title = title
   this.author = author
   this.pages = pages
   this.status = status
+
+  // returns a <tr> element with the book info
+  this.toTableRow = function () {
+    // returns a <td> element with input text
+    function createTableData(text) {
+      let node = document.createElement('td')
+      node.textContent = text
+      return node
+    }
+
+    let row = document.createElement('tr')
+
+    row.appendChild(createTableData(this.title))
+    row.appendChild(createTableData(this.author))
+    row.appendChild(createTableData(this.pages))
+    row.appendChild(createTableData(this.status))
+
+    return row
+  }
 }
 
-console.log(new Book('1984', 'George Orwell', 120, true))
+library.push(new Book('1984', 'George Orwell', 120, true))
+library.push(new Book('I Robot', 'Isaac Asimov', 150, false))
+
+// select shelve container
+const shelve = document.getElementById('shelve')
+
+// appends lirary books to the shelve container
+for (const book of library) {
+  shelve.appendChild(book.toTableRow())
+}
