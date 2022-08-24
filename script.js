203bf99b28e9235b51833c556e92d16dbfdf8464
Nicolás Órdenes <nicordenes@gmail.com>
2022-08-24 14:25:23 -0400

    Add Book constructor

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('hello from scrip.js')
+function Book(title, author, pages, status) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.status = status
+}
+
+console.log(new Book('1984', 'George Orwell', 120, true))
