75005a0a2cfb2e210346d8070ba87210ea86e13f
Nicolás Órdenes <nicordenes@gmail.com>
2022-08-25 12:06:44 -0400

    style: apply prettier default formatting

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 function Library(selector) {
-  this.node = document.getElementById(selector)
+  this.node = document.getElementById(selector);
   this.books = [];
 
-  this.addBook = (book) => this.books.push(book)
+  this.addBook = (book) => this.books.push(book);
 
   this.removeBookByIndex = (index) => {
-    this.books.splice(index, 1)
-    this.render()
-  }
+    this.books.splice(index, 1);
+    this.render();
+  };
 
   this.toggleStatusByIndex = (index) => {
-    book = this.books[index]
-    book.status = !book.status
-    this.render()
-  }
+    book = this.books[index];
+    book.status = !book.status;
+    this.render();
+  };
 
   // deletes all books in the table
   this.reset = () => {
-    let rows = Array.from(this.node.rows)
-    rows.forEach(row => row.remove())
-  }
+    let rows = Array.from(this.node.rows);
+    rows.forEach((row) => row.remove());
+  };
 
   // append all books to the table
   this.render = () => {
-    this.reset()  // avoid duplicates
+    this.reset(); // avoid duplicates
     for (let i = 0; i < this.books.length; i++) {
       const book = this.books[i];
-      let tr = document.createElement('tr')
+      let tr = document.createElement("tr");
 
       // FIXME library shouldn't have knowledge of representation
       tr.innerHTML = `
@@ -34,7 +34,7 @@ function Library(selector) {
       <td>${book.author}</td>
       <td>${book.pages}</td>
       <td></td>
-      <td></td>`
+      <td></td>`;
 
       // add book status checkbox
       node = tr.children[3]
@@ -43,24 +43,24 @@ function Library(selector) {
       } else {
         node.innerHTML = `
           <label for="status_${i}">mark as read</label>
-          <input type="checkbox" name="status" id="status_${i}" data-book-index=${i}>`
+          <input type="checkbox" name="status" id="status_${i}" data-book-index=${i}>`;
       }
 
       // add delete button
-      node = tr.children[4]
-      node.innerHTML = `<button data-book-index=${i}>delete</button>`
+      node = tr.children[4];
+      node.innerHTML = `<button data-book-index=${i}>delete</button>`;
 
       // add row to table
-      this.node.appendChild(tr)
+      this.node.appendChild(tr);
     }
-  }
+  };
 }
 
 function Book(title, author, pages, status) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.status = status
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
 }
 
 // handle input elements
@@ -68,52 +68,52 @@ function formInput(name) {
   this.node = document.getElementById(name);
 
   this.value = () => this.node.value;
-  this.reset = () => this.node.value = "";
+  this.reset = () => (this.node.value = "");
 }
 
 // handle radio buttons
 function formRadio(name) {
-  this.node = document.getElementsByName('book_status')
+  this.node = document.getElementsByName("book_status");
 
   this.value = () => {
     for (const radio of this.node) {
       if (radio.checked) {
-        return radio.value == "true" ? true : false
+        return radio.value == "true" ? true : false;
       }
     }
-  }
+  };
 
   this.reset = () => {
     for (const radio of this.node) {
-      radio.checked = false
+      radio.checked = false;
     }
-  }
+  };
 }
 
 // create Library
-const library = new Library('shelve')
+const library = new Library("shelve");
 
 // select form inputs
-let title = new formInput('title');
-let author = new formInput('author');
-let pages = new formInput('pages');
-let status = new formRadio('book_status');
+let title = new formInput("title");
+let author = new formInput("author");
+let pages = new formInput("pages");
+let status = new formRadio("book_status");
 
 // submit buttons
-let form = document.querySelector('#newBookForm')
+let form = document.querySelector("#newBookForm");
 
-form.addEventListener("submit", e => {
-  e.preventDefault()
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
   let book = new Book(
     title.value(),
     author.value(),
     pages.value(),
     status.value()
-  )
+  );
 
-  library.addBook(book)
-  library.render()
+  library.addBook(book);
+  library.render();
 
   title.reset();
   author.reset();
@@ -122,15 +122,15 @@ form.addEventListener("submit", e => {
 });
 
 // handle delete button clicks
-const table = document.querySelector('table')
-table.addEventListener('click', e => {
+const table = document.querySelector("table");
+table.addEventListener("click", (e) => {
   if (e.target.type == "submit") {
     let tr = e.target.parentElement.parentElement
     library.removeBookByIndex(tr.dataset.bookIndex)
   }
-})
+});
 
-table.addEventListener('change', e => {
-  index = e.target.dataset.bookIndex
-  library.toggleStatusByIndex(index)
-})
+table.addEventListener("change", (e) => {
+  index = e.target.dataset.bookIndex;
+  library.toggleStatusByIndex(index);
+});
